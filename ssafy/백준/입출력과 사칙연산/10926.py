490bbaa8af188ef4836b14a315d6f9ce06a9fad0
JYeol97 <a01092201761@gmail.com>
2024-07-16 19:50:09 +0900

    입출력과 사칙연산

---
+++ ssafy/백준/입출력과 사칙연산/10926.py
@@ -0,0 +1,20 @@
+# def check_id():
+#     id = input()
+#     if id == "joonas":
+#         print("joonas??!")
+#     elif id == "baekjoon":
+#         print("baekjoon??!")
+#     else:
+#         return id
+# print(check_id)
+
+# input("joonas")
+# id = "joonas"
+# def check_id():
+#     id = input()
+#     if id == id:
+#         print(id + "??!")
+
+# def check_id():
+#     id = input()
+#     print(id + "??!")
